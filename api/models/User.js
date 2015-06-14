098424577c656d08683f6aed05278d180408d777
Paul Wood III <paul.wood@walkin.com.cn>
2015-06-14 12:46:55 +0000

    test user object dumbly

--- api/models/User.js
+++ api/models/User.js
@@ -1,9 +1,9 @@
 /**
- * User.js
- *
- * @description :: This model represents users logging onto the webapp
- * @docs        :: http://sailsjs.org/#!documentation/models
- */
+* User.js
+*
+* @description :: This model represents users logging onto the webapp
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
 var bcrypt = require('bcrypt');
 
 module.exports = {
@@ -27,22 +27,16 @@ module.exports = {
     },
     beforeCreate: function(user, cb) {
         bcrypt.genSalt(10, function(err, salt) {
-            if (err) {
-                console.log(err);
-                cb(err);
-            }
-            else {
-                bcrypt.hash(user.password, salt, function(err, hash) {
-                    if (err) {
-                        console.log(err);
-                        cb(err);
-                    }
-                    else {
-                        user.password = hash;
-                        cb();
-                    }
-                });
-            }
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) {
+                    console.log(err);
+                    cb(err);
+                } else {
+                    user.password = hash;
+                    cb(null,user);
+                }
+            });
         });
-    }
+    }    
 };
+
