0c1471599f125da4aa20109c65e2628fff87de32
Paul Wood III <paul.wood@walkin.com.cn>
2015-06-22 10:08:53 +0000

    a few more scripting changes

--- deploy_scripts/AfterInstall.sh
+++ deploy_scripts/AfterInstall.sh
@@ -3,13 +3,11 @@ pushd .
 cd /home/ec2-user/quotes
 #need the pushd and popd because codedeploy with cd
 #will break the wholeagent
-git clone git://github.com/creationix/nvm.git ~/.nvm;
-source ~/.nvm/nvm.sh;
-echo "source ~/.nvm/nvm.sh" >> ~/.bash_profile;
 nvm install v0.10.35;
 nvm alias default v0.10.35;
+nvm use default
 npm install
-npm install pm2 -g
+npm install -g pm2 
 npm i -g grunt-cli
 popd
 
