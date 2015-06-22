264ead54d803426bcd0b7524edac58888c4455ba
Paul Wood III <paul.wood@walkin.com.cn>
2015-06-22 12:35:18 +0000

    tried to silence some failure warning on initial setup of the deployment

--- deploy_scripts/BeforeInstall.sh
+++ deploy_scripts/BeforeInstall.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rm -rf /home/ec2-user/quotes_archive 2>/dev/null
+mkdir /home/ec2-user/quotes_archive 2>/dev/null
+mkdir /home/ec2-user/quotes 2>/dev/null
 mv /home/ec2-user/quotes /home/ec2-user/quotes_archives 2>/dev/null
