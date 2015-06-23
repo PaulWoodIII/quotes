eb829cd8d1684a273c773c7278e1b066758b177a
Paul Wood III <paul.wood@walkin.com.cn>
2015-06-23 01:51:13 +0000

    pm2 starts the app in a production environment

--- deploy_scripts/ApplicationStart.sh
+++ deploy_scripts/ApplicationStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pushd .
 cd /home/ec2-user/quotes
-env PATH=$PATH:/home/ec2-user/.nvm/v0.10.35/bin pm2 start app.js
+env PATH=$PATH:/home/ec2-user/.nvm/v0.10.35/bin pm2 start app.js -x -- --prod
 popd
